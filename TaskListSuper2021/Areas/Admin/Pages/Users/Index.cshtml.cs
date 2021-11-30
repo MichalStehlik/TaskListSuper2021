d4a487ad6897b84904eddefaf33a97ed42ad259c
Michal Stehlík <michal.stehlik@pslib.cz>
2021-11-30 18:16:55 +0100

    feature update

---
+++ TaskListSuper2021/Areas/Admin/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TaskListSuper2021.Data;
+using TaskListSuper2021.Models;
+
+namespace TaskListSuper2021.Areas.Admin.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TaskListSuper2021.Data.ApplicationDbContext _context;
+
+        public IndexModel(TaskListSuper2021.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ApplicationUser> Users { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Users = await _context.Users.ToListAsync();
+        }
+    }
+}
---
+++ TaskListSuper2021/ViewModels/UserIM.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskListSuper2021.ViewModels
+{
+    public class UserIM
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
